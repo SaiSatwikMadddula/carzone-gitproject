05d41b0a59710e03319458c0d3a99c827566ce2b
SaiSatwikMaddula <saisatwikmaddula@gmail.com>
2022-01-01 19:44:12 +0530

    Added search.html

--- cars/views.py
+++ cars/views.py
@@ -13,9 +13,22 @@ def cars(request):
     }
     return render(request,'cars/cars.html',data)
 
+
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
     data = {
         'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html', data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+    data = {
+        'cars': cars,
+    }
+    return render(request,'cars/search.html',data)
