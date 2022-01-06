66f07939fe1ef0a4c469462652df502c8c7eb003
SaiSatwikMaddula <saisatwikmaddula@gmail.com>
2022-01-06 22:41:35 +0530

    Added messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
@@ -6,7 +7,11 @@ def login(request):
 
 
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method == 'POST':
+        messages.error(request, 'This is error message')
+        return redirect('register')
+    else:
+        return render(request,'accounts/register.html')
 
 
 def logout(request):
